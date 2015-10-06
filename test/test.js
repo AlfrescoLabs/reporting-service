b9108f063be61cefdd06d81b1e3e6b1a7afee88c
Michael Suzuki <msuzuki@mbp-msuzuki-0915.home>
2015-10-06 15:19:06 +0100

    refactor test and removed duplication

--- test/test.js
+++ test/test.js
@@ -1,21 +1,21 @@
-var assert = require('assert');
-var expect = require('expect');
-var superagent = require('superagent');
-var should = require('should');
-var app = require('../app');
-var db = require('mongoskin').db('mongodb://localhost:27017/testplatform');
+var assert = require('assert')
+var expect = require('expect')
+var superagent = require('superagent')
+var should = require('should')
+var app = require('../app')
+var db = require('mongoskin').db('mongodb://localhost:27017/testplatform')
 
 
 describe('reporting/api/alfresco/5.1', function() {
     it('Should get data and store only one entery per day',function(done){
       this.timeout(15000); // Setting a longer timeout
       //Call api to update backend twice, expect one entry as this as an upsert op.
-      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1').end();
-      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1').end();
-      var today = new Date();
-      var parsedDate =  today.getDate()+ "/" + (new Number(today.getMonth()) + 1) + "/" + today.getFullYear();
+      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1').end()
+      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1').end()
+      var today = new Date()
+      var parsedDate =  today.getDate()+ "/" + (new Number(today.getMonth()) + 1) + "/" + today.getFullYear()
       db.collection('report').find({'dateDisplay':parsedDate}).toArray(function(err, result) {
-        should(1).be.equal(result.length);
+        should(1).be.equal(result.length)
         verifyModel(result[0].open)
         done();
       });
@@ -28,20 +28,20 @@ describe('reporting/api/alfresco/5.1/01/01/2015', function() {
       var parsedDate =  targetDate.getDate()+ "/" + (new Number(targetDate.getMonth()) + 1) + "/" + targetDate.getFullYear()
       //Call api to update backend twice, expect one entry as this as an upsert op.
       superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/01/01/2015').end(function(err,res){
-        assert(res.status === 200);
-        should.equal('1/1/2015', res.body.dateDisplay);
+        assert(res.status === 200)
+        should.equal('1/1/2015', res.body.dateDisplay)
         verifyDB(parsedDate)
       })
       superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/1/1/2015').end(function(err,res){
-        assert(res.status === 200);
-        should.equal('1/1/2015', res.body.dateDisplay);
+        assert(res.status === 200)
+        should.equal('1/1/2015', res.body.dateDisplay)
         verifyDB(parsedDate)
         done()
       })
     })
     function verifyDB(parsedDate){
       db.collection('report').find({'dateDisplay':parsedDate}).toArray(function(err, result) {
-      should(1).be.equal(result.length);
+      should(1).be.equal(result.length)
       })
     }
 });
@@ -50,16 +50,16 @@ describe('reporting/api/alfresco/5.1/status', function() {
   it('should return open and closed jira issues from mongo', function(done) {
     superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/status').end(
       function(err, res) {
-        assert.ifError(err);
-        assert(res.status === 200);
-        var response = res.body;
+        assert.ifError(err)
+        assert(res.status === 200)
+        var response = res.body
         //Skip first result as it will be data from a negative test.
-        var json = response[1];
-        json.should.have.property('date');
-        json.should.have.property('dateDisplay');
-        verifyModel(json.open);
-        verifyModel(json.close);
-        done();
+        var json = response[1]
+        json.should.have.property('date')
+        json.should.have.property('dateDisplay')
+        verifyModel(json.open)
+        verifyModel(json.close)
+        done()
       });
   });
 });
@@ -67,33 +67,31 @@ describe('reporting/api/alfresco/5.1/new/defects', function() {
   it('should only return open jira issues from mongo', function(done) {
     superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/new/defects').end(
       function(err, res) {
-        assert.ifError(err);
-        assert(res.status === 200);
-        var response = res.body;
-        var json = response[0];
-        json.should.have.property('date');
-        json.open.should.have.property('blocker');
-        json.open.should.have.property('critical');
-        should.equal(json.open.critical+json.open.blocker, json.open.count)
-        should.not.exist(json.close);
-        json.should.have.property('open');
-        json.open.should.have.property('count');
-        json.open.should.have.property('issues');
-        done();
+        assert.ifError(err)
+        assert(res.status === 200)
+        var response = res.body
+        var json = response[0]
+        json.should.have.property('date')
+        should.not.exist(json.close)
+        json.should.have.property('open')
+
+        verifyModel(json.open)
+        done()
       });
   });
 });
 function verifyModel(json){
-  json.should.have.property('count');
-  json.should.have.property('critical');
-  json.should.have.property('blocker');
-  json.should.have.property('issues');
-  var issues = json.issues;
-  if(typeof issues !== 'undefined'){
+  json.should.have.property('count')
+  json.should.have.property('critical')
+  json.should.have.property('blocker')
+  json.should.have.property('issues')
+  var issues = json.issues
+
+  if(json.issues.length > 0){
     should.equal(json.critical+json.blocker, json.count)
-    issues[0].should.have.property('id');
-    issues[0].should.have.property('link');
-    issues[0].should.have.property('type');
+    json.issues[0].should.have.property('id')
+    issues[0].should.have.property('link')
+    issues[0].should.have.property('type')
   } else {
     should.equal(0, json.count)
   }
