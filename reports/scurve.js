cc172e5bacf69c1479040c1af0b887a88198ec84
Michael Suzuki <msuzuki@mbp-msuzuki-0915.home>
2015-11-09 11:54:18 +0000

    refactor data model and chart

--- reports/scurve.js
+++ reports/scurve.js
@@ -6,12 +6,16 @@ var config = require('../config')
 var db = require('mongoskin').db(config.mongo)
 module.exports = {
     getReport: function(req,res){
-        var data = [{"id":1,"date":"1/11/2015","tc":"66"},
-                    {"id":2,"date":"2/11/2015","tc":"44"},
-                    {"id":3,"date":"3/11/2015","tc":"46"},
-                    {"id":4,"date":"4/11/2015","tc":"56"},
-                    {"id":5,"date":"5/11/2015","tc":"77"},
-                    {"id":6,"date":"6/11/2015","tc":"100"}];
+        var data = {tc:100,
+                    targetDate:"6/11/2015",
+                    data:
+                    [{"id":1,"dateDisplay":"1/11/2015","tc":"66"},
+                    {"id":2,"dateDisplay":"2/11/2015","tc":"44"},
+                    {"id":3,"dateDisplay":"3/11/2015","tc":"46"},
+                    {"id":4,"dateDisplay":"4/11/2015","tc":"56"},
+                    {"id":5,"dateDisplay":"5/11/2015","tc":"77"},
+                    {"id":6,"dateDisplay":"6/11/2015","tc":"100"}]
+                };
         res.send(data)
     }
 
