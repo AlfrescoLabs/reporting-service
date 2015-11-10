b1ee1a97ee7bb74bf0b871c090400a2f633dcb92
Michael Suzuki <msuzuki@MBP-MSUZUKI-0915.local>
2015-11-10 15:00:48 +0000

    change to handle dates instead of number of days

--- reports/scurve.js
+++ reports/scurve.js
@@ -10,7 +10,9 @@ module.exports = {
         var startDate = 1/11/2015
         var endDate = 10/11/2015
         var totaclTC = 100
-        var data = {"totalTC":totaclTC,//3593
+        var data = {"product":"5.1",
+                    "run":"1",
+                    "totalTC":totaclTC,//3593
                     "targetDate":"10/11/2015",//09/12/2015
                     "startDate":"1/11/2015",//16/11/2015
                     data:
@@ -40,26 +42,36 @@ module.exports = {
         var scurve = getScurve(startDate,endDate,totalTC)
         res.send(scurve)
     },
-
-    parseDateString : function(date){
-        var y = date.split('/')[2];
-        var m = date.split('/')[1] - 1;
-        var d = date.split('/')[0];
-        return new Date(y,m, d);
-    }
+}
+function parseDateString(date){
+    var y = date.split('/')[2];
+    var m = date.split('/')[1] - 1;
+    var d = date.split('/')[0];
+    return new Date(y,m,d);
+}
+function dateToString(date){
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    var d = date.getDate();
+    return d + "/" + m  + "/" + y;
 }
 function getScurve(startDate,endDate,totaclTC){
     var results=[]
-    var days = 9
+    var days = diff(endDate,startDate);
+    var start = parseDateString(startDate);
       for(i = 0 ; i< days; i++){
+          var dateDisplay = new Date(start);
+          var t = dateDisplay.setDate(dateDisplay.getDate() +  i)
+          var formattedDate = dateToString(new Date(t))
           //DE = Current Day Number of Effort / Total Days in Effort
           var currentDay = i+1;
           var DE = i / days;
           var e = 2.718281828459045
           var result = DE / (DE + Math.pow(e,(3 - 8 * DE)));
           var estimatedTC = Math.round(result * totaclTC);
-          results.push({"day" : currentDay, "tc" : estimatedTC})
+          results.push({"day" : formattedDate, "tc" : estimatedTC})
       }
+
       function diff( date1, date2 ) {
         var day1 = parseDateString(date1);
         var day2 = parseDateString(date2);
--- test/scurve.js
+++ test/scurve.js
@@ -8,22 +8,29 @@ var db = require('mongoskin').db(config.mongo)
 var async = require('async')
 
 var expectedScurve = [
-  { day: 1, tc: 0 },
-  { day: 2, tc: 1 },
-  { day: 3, tc: 6 },
-  { day: 4, tc: 19 },
-  { day: 5, tc: 44 },
-  { day: 6, tc: 70 },
-  { day: 7, tc: 87 },
-  { day: 8, tc: 95 },
-  { day: 9, tc: 98 } ];
+{ day: "1/11/2015", tc: 0},
+{ day: "2/11/2015", tc: 1},
+{ day: "3/11/2015", tc: 2},
+{ day: "4/11/2015", tc: 5},
+{ day: "5/11/2015", tc: 10},
+{ day: "6/11/2015", tc: 19},
+{ day: "7/11/2015", tc: 33},
+{ day: "8/11/2015", tc: 49},
+{ day: "9/11/2015", tc: 65},
+{ day: "10/11/2015",tc: 78},
+{ day: "11/11/2015",tc: 87},
+{ day: "12/11/2015",tc: 93},
+{ day: "13/11/2015",tc: 96},
+{ day: "14/11/2015",tc: 98},
+{ day: "15/11/2015",tc: 99}
+]
 //////////////////// SCURVE Projection
 describe('reporting/api/alfresco/:version/scurve/:startDate/:endDate/:totalTC',function(done){
     it('Should get array of scurve projection based on start date,end date and total test cases', function(done) {
       this.timeout(15000); // Setting a longer timeout
-      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/scurve/1/11/2015/10/11/2015/100').end(function(err, res) {
+      superagent.get('http://localhost:3000/reporting/api/alfresco/5.1/scurve/1/11/2015/16/11/2015/100').end(function(err, res) {
         assert(res.status === 200)
-        expect(res.body).toEqual(expectedScurve)
+        expect(expectedScurve).toEqual(res.body)
         done()
       });
     });
