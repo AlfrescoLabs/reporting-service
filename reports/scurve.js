2fa5ce1fc7b3e13110a115a93e232b85081f66af
Michael Suzuki <msuzuki@MBP-MSUZUKI-0915.local>
2015-11-10 11:37:03 +0000

    implemented s-curve function

--- reports/scurve.js
+++ reports/scurve.js
@@ -8,13 +8,14 @@ module.exports = {
     getReport: function(req,res){
         var data = {tc:100,
                     targetDate:"10/11/2015",
+                    startDate:"1/11/2015",
                     data:
-                    [{"id":1,"dateDisplay":"1/11/2015","tc":"66"},
-                    {"id":2,"dateDisplay":"2/11/2015","tc":"44"},
-                    {"id":3,"dateDisplay":"3/11/2015","tc":"46"},
-                    {"id":4,"dateDisplay":"4/11/2015","tc":"56"},
-                    {"id":5,"dateDisplay":"5/11/2015","tc":"77"},
-                    {"id":6,"dateDisplay":"6/11/2015","tc":"100"}]
+                    [{"id":1,"day":"1","tc":"66"},
+                    {"id":2,"day":"2","tc":"44"},
+                    {"id":3,"day":"3","tc":"46"},
+                    {"id":4,"day":"4","tc":"56"},
+                    {"id":5,"day":"5","tc":"77"},
+                    {"id":6,"day":"6","tc":"100"}]
                 };
         res.send(data)
     }
