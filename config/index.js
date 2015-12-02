8544627d7bc4eb49e1302dbbf0f46aa4d3fe5189
Sergiu-Alexandru Vidrascu <Sergiu.Vidrascu@ness.com>
2015-12-02 18:11:10 +0200

    added prod configuration, jira details are picked up from environment variables

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@ var env = process.env.NODE_ENV || 'development'
 switch (env) {
   case 'development':
   case 'test':
+  case 'prod':
     config = require('./config.' + env);
     break;
   default:
