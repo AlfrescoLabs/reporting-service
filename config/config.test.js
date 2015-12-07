9226e2f070d5354121515f7249ef977f5bbdf457
Sergiu Vidrascu <vsergiu@hotmail.com>
2015-12-07 12:15:26 +0200

    Fixed tests, removed space from config jira_url

--- config/config.test.js
+++ config/config.test.js
@@ -1,6 +1,5 @@
 var config = require('./config.global')
 config.env = 'test'
 config.mongo = 'mongodb://localhost:27017/test-testplatform'
-config.jira.url = ' https://issuestest.alfresco.com'
-config.jira.authentication = process.env.JIRA_AUTH
+config.jira.url = 'https://issuestest.alfresco.com'
 module.exports = config
