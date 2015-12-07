f4963174e6f5ec71f730e22b3ebb2be0368ee461
Sergiu Vidrascu <vsergiu@hotmail.com>
2015-12-07 11:22:31 +0200

    Update config.js, added auth posibility

--- config/config.test.js
+++ config/config.test.js
@@ -2,4 +2,5 @@ var config = require('./config.global')
 config.env = 'test'
 config.mongo = 'mongodb://localhost:27017/test-testplatform'
 config.jira.url = ' https://issuestest.alfresco.com'
+config.jira.authentication = process.env.JIRA_AUTH
 module.exports = config
