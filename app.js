99a8695dd5002a8001abea67d8c9cc51071f4c09
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-03-14 15:52:56 +0000

    enabled CORS

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.set('view engine', 'html')
 app.use(bodyParser.urlencoded({'extended': 'true'}))    // parse application/x-www-form-urlencoded
 app.use(bodyParser.json())                              // parse application/json
 app.use(express.static(path.join(__dirname, 'public'))) // Include static assets. Not advised for production
+app.use(function(req, res, next) {                      
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
 app.use('/reporting', routes)
 app.listen(3000)
 module.exports = app
