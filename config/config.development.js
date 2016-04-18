29cc301aca7bcb15f4c825bc44fefef5711b6f5e
Sergiu Vidrascu <vsergiu@hotmail.com>
2016-04-18 15:57:53 +0300

    Update config.development.js

--- config/config.development.js
+++ config/config.development.js
@@ -1,4 +1,6 @@
 var config = require('./config.global')
 config.env = 'development'
 config.mongo = process.env.NODE_MONGO || 'mongodb://localhost:27017/testplatform'
+config.jira.url = process.env.JIRA_URL || 'https://issuestest.alfresco.com'
+config.jira.authentication = process.env.JIRA_AUTH || ''
 module.exports = config
