33a0093047cd67ca9a8def471947866c55d209b7
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-04-19 15:10:25 +0100

    remove logging

--- reports/summary-report.js
+++ reports/summary-report.js
@@ -6,7 +6,6 @@ var db = require('mongoskin').db(config.mongo)
         var name = req.params.product
         var version = req.params.version
         db.collection('reports').findOne({"name":name,"version":version}, function(err, result) {
-            console.log(result)
             if(result === null) res.send({msg:"no data found"})
             res.send(result.data)
         });
