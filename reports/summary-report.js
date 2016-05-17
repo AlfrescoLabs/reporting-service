66525271f4429c905c915ba4510c20578ff351b8
michaelsuzukisagi <michael.suzuki@alfresco.com>
2016-05-17 12:02:37 +0100

    handle empty data

--- reports/summary-report.js
+++ reports/summary-report.js
@@ -6,8 +6,11 @@ var db = require('mongoskin').db(config.mongo)
         var name = req.params.product
         var version = req.params.version
         db.collection('reports').findOne({"name":name,"version":version}, function(err, result) {
-            if(result === null) res.send({msg:"no data found"})
-            res.send(result.data)
+            if(result === null){
+                res.send({msg:"no data found"})
+            } else {
+                res.send(result.data)
+            }
         });
     }
 }
